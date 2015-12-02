e87ad0540bce61fefcb9039c63073e761c894332
José Ignacio Escribano <jiep12345@hotmail.com>
2015-12-02 15:59:12 +0100

    Hechas cuestiones y código R

--- 2. Redes neuronales/caso_2.R
+++ 2. Redes neuronales/caso_2.R
@@ -1,20 +1,21 @@
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------
 # Cuestiones de evaluación
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------
 
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------
 # Cuestión 1
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------
 
-# Dibujamos la función y = x*sen(x) en [-2π, 2π] con 200 puntos
+# Dibujamos la función y = x*sen(x) en [-2π, 2π] 
+# con 200 puntos
 points = 200
 x <- seq(-2*pi, 2*pi, length = points)
 y <- x*sin(x)
 plot(x,y, type='l', main = 'y = x*sin(x)')
 
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------
 # Cuestión 2
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------
 
 # Ajustamos los datos según un modelo linear
 linear.model <- lm(y~x)
@@ -28,20 +29,23 @@ abline(h=-1, col=2)
 plot(linear.model,1)
 plot(linear.model,2)
 
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------
 # Cuestión 3
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------
 
 # Cargamos la librería
 library(nnet)
 
-# Definimos función para ejecutar varias veces (hasta un máximo de 10) 
-# el comando nnet y devuelve el ajuste  que tenga el error menor tenga
+# Definimos función para ejecutar varias veces (hasta un 
+# máximo de 10) el comando nnet y devuelve el ajuste  que 
+# tenga el error menor tenga
 min.nn <- function(x,y, iter=10, neurals=5){
   min_value <- 10e6 # Infinito
   actual_fit <- NULL
   for(i in seq(1, iter)){
-    fit.nn <- nnet(x,y, rang=0.1, size=neurals, linout=T, maxit=10000, trace = FALSE)
+    fit.nn <- nnet(x,y, rang=0.1, size=neurals, linout=T, 
+        maxit=10000)
+    cat(fit.nn$value, "\n")
     if(fit.nn$value < min_value){
       min_value <- fit.nn$value
       actual_fit <- fit.nn
@@ -50,18 +54,24 @@ min.nn <- function(x,y, iter=10, neurals=5){
   return(actual_fit)
 }
 
-# Comparamos la función calculada con la función original según el número de
-# neuronas en la capa oculta (desde 1 hasta 10)
-nf<-layout(matrix(c(1,2,3,4,5,6,7,8,9,10,11,11,11,11,11), ncol=5, byrow=TRUE))
+# Comparamos la función calculada con la función original 
+# según el número de neuronas en la capa oculta 
+# (desde 1 hasta 10)
+nf<-layout(matrix(c(1,2,3,4,5,6,7,8,9,10,11,11,11,11,11), 
+    ncol=5, byrow=TRUE))
 par(mai= rep(0.5, 4))
+predictions <- vector()
 for(neurals in seq(1,10)){
+  
+  # Curva ajustada con el menor error entre varias ejecuciones 
+  # de la función nnet
   min <- min.nn(x, y, 10, neurals)
   
-  # Curva ajustada con el menor error entre varias ejecuciones de la
-  # función nnet
+  # Predicción de los valores a partir de la red calculada
   predict.nn<-predict(min, as.matrix(x))
   
-  # Representamos la función predicha junto con la función original
+  # Representamos la función predicha junto con la función 
+  # original
   if(neurals == 1) 
     neurals_plot <-  "neurona" 
   else 
@@ -73,8 +83,12 @@ for(neurals in seq(1,10)){
 
 par(mai=c(0,0,0,0))
 plot.new()
-legend(x="center", ncol=2,legend=c("Función predicha", "Función original"),
-        title="Leyenda", lty = c(1, NA), pch = c(NA, 'O'), col=c('red', 'blue'))
+legend(x="center", ncol=2,
+    legend=c("Función predicha", "Función original"),
+    title="Leyenda", 
+    lty = c(1, NA), 
+    pch = c(NA, 'O'), 
+    col=c('red', 'blue'))
 
 # Cargamos la librería neuralnet
 library(neuralnet)
@@ -83,8 +97,7 @@ library(neuralnet)
 
 par(mfrow=c(1,2))
 for(neurals in c(1,3)){
-  curve.nn <- neuralnet(y~x, cbind(x,y), hidden=neurals, rep=2, stepmax=10e6)
+  curve.nn <- neuralnet(y~x, cbind(x,y), hidden=neurals, 
+      rep=1, stepmax=10e6)
   plot.nn(curve.nn)
 }
-
-
