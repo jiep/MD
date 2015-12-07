3090ec5def9a01a5bba778fd196129aa6f7a9436
José Ignacio Escribano <jiep12345@hotmail.com>
2015-12-07 11:56:09 +0100

    Memoria hecha

--- 4. Árboles de decisión/caso_4.R
+++ 4. Árboles de decisión/caso_4.R
@@ -11,20 +11,27 @@ library(datasets)
 library(rpart)
 
 # Calculamos el árbol sin podar
-iris.tree <- rpart(formula = Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, method="class", data=iris)
+iris.tree <- rpart(
+  formula = Species ~ Sepal.Length + Sepal.Width + 
+    Petal.Length + Petal.Width, method="class", data=iris)
 
 # Representamos el árbol sin podar
 plot(iris.tree)
 text(iris.tree)
 
 # Calculamos el árbol ajustando el coeficiente de complejidad
-iris.tree2 <- rpart(formula = Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, method="class", data=iris, cp=0.0000001)
+iris.tree2 <- rpart(
+  formula = Species ~ Sepal.Length + Sepal.Width + 
+    Petal.Length + Petal.Width, method="class", 
+    data=iris, cp=0.0000001)
 
-# Representamos el gráfico con el error, el número de nodos y el coeficiente de complejidad
+# Representamos el gráfico con el error, el número de nodos 
+# y el coeficiente de complejidad
 plotcp(iris.tree2)
 
 # Calculamos el cp óptimo para del árbol, antes de podar
-cpar <- iris.tree2$cptable[which.min(iris.tree2$cptable[,"xerror"]), "CP"]
+cpar <- iris.tree2$cptable[
+  which.min(iris.tree2$cptable[,"xerror"]), "CP"]
 
 # Podamos el árbol con el valor de cp que acabamos de calcular
 iris.tree3 <- prune(iris.tree2, cp=cpar)
@@ -50,15 +57,4 @@ text(spam7.tr)
 
 # Resumen del árbol calculado
 summary(spam7.tr)
-
-spam7a.tree <- rpart(yesno ~ crl.tot + dollar + bang + money + n000 + make, method = "class", data = spam7, cp = 0.0000001)
-
-plotcp(spam7a.tree)
-
-cpar <- spam7a.tree$cptable[which.min(spam7a.tree$cptable[,"xerror"]), "CP"]
-
-spam7b.tree <- prune(spam7a.tree, cp=cpar)
-
-plot(spam7b.tree, uniform=TRUE)
-text(spam7b.tree, cex=0.75)
 
